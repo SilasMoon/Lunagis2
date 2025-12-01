f5c867faedd728af9e0e031a35ea6b2f8877ee96
Geoffray Doignon <geoffray.doignon@gmail.com>
2025-12-01 09:52:55 +0000

    bug fix for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,18 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      base: process.env.BASE_URL || env.BASE_URL || '/Lunagis/',
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    base: process.env.BASE_URL || env.BASE_URL || '/Lunagis2/',
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    }
+  };
 });
